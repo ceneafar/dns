47b7cae29a893606f56c70c7ccebe6d6249767aa
ceneafar <carteaga460@gmail.com>
2023-03-31 03:40:14 +0000

    php code can be executed in the view

--- controller.php
+++ controller.php
@@ -1,34 +1,34 @@
 <?php
 
-require "model.php";
-$url = $_POST["url"];
+if(isset($_POST["btn"])){
+	require "model.php";
+	$url = $_POST["url"];
+	$dns = new Dns($url);
 
-$dns = new Dns($url);
+	$a_record = $dns->dns_a();
+	$mx_record = $dns->dns_mx();
+	$txt_record = $dns->dns_txt();
+	$ns_record = $dns->dns_ns();
+	$aaaa_record = $dns->dns_aaaa();
+	$soa_record = $dns->dns_soa();
 
-$a_record = $dns->dns_a();
-$mx_record = $dns->dns_mx();
-$txt_record = $dns->dns_txt();
-$ns_record = $dns->dns_ns();
-$aaaa_record = $dns->dns_aaaa();
-$soa_record = $dns->dns_soa();
-
-print_r($a_record);
-echo "<br>";
-echo "<br>";
-print_r($mx_record);
-echo "<br>";
-echo "<br>";
-print_r($txt_record);
-echo "<br>";
-echo "<br>";
-print_r($ns_record);
-echo "<br>";
-echo "<br>";
-print_r($aaaa_record);
-echo "<br>";
-echo "<br>";
-print_r($soa_record);
-
+	print_r($a_record);
+	echo "<br>";
+	echo "<br>";
+	print_r($mx_record);
+	echo "<br>";
+	echo "<br>";
+	print_r($txt_record);
+	echo "<br>";
+	echo "<br>";
+	print_r($ns_record);
+	echo "<br>";
+	echo "<br>";
+	print_r($aaaa_record);
+	echo "<br>";
+	echo "<br>";
+	print_r($soa_record);
+}
 
 
 
